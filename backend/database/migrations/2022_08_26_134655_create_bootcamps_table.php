c65290e6c7a26abbfc918511c71431128c6ef64c
Daniel <ddaza681@gmail.com>
2022-08-26 09:35:11 -0500

    Se creo laravel

---
+++ backend/database/migrations/2022_08_26_134655_create_bootcamps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bootcamps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->text('description');
+            $table->string('phone',20);
+            $table->string('website',60);
+            $table->float('average_rating', 2, 1);
+            $table->float('average_cost', 8, 2);
+            $table->foreingId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bootcamps');
+    }
+};
---
+++ backend/database/migrations/2022_08_26_134718_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->integer('weeks');
+            $table->float('enroll_cost', 8, 2);
+            $table->timestamps();
+            $table->string('minumun_skill',20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
